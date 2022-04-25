743dfcbda140785540dbd6d53d6183a7cb759533
umekaf <kafleumesh@gmail.com>
2022-04-25 23:26:43 +0200

    Added required validations and associations for shopper and merchant mmodel

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,8 @@
 class Merchant < ApplicationRecord
+  has_many :orders, dependent: :destroy
+  has_many :merchant_disbursements, dependent: :destroy
+  
+  validates :email, :name, :cif, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :cif, uniqueness: { case_sensitive: false }
 end
--- app/models/shopper.rb
+++ app/models/shopper.rb
@@ -1,2 +1,7 @@
 class Shopper < ApplicationRecord
+  has_many :orders, dependent: :destroy
+  
+  validates :email, :name, :nif, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :nif, uniqueness: { case_sensitive: false }
 end
